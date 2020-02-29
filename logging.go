b822d94f9ae7025fcb6de1ed0ee9c1f6cd5f4362
myback <54638513+myback@users.noreply.github.com>
2020-03-01 00:02:45 +0700

    access_log use DebugSet

--- logging.go
+++ logging.go
@@ -60,7 +60,7 @@ func (l *Logger) Fatal(msg string) {
 	os.Exit(127)
 }
 
-func (l *Logger) Access(next http.Handler, logHeader bool) http.Handler {
+func (l *Logger) Access(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var u, h string
 		login, _, ok := r.BasicAuth()
@@ -70,7 +70,7 @@ func (l *Logger) Access(next http.Handler, logHeader bool) http.Handler {
 			u = "-"
 		}
 
-		if logHeader {
+		if l.DebugSet {
 			var hl []string
 			for k, v := range r.Header {
 				hl = append(hl, fmt.Sprintf(`http_%s="%s"`, k, v[0]))
