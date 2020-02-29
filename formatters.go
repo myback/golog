c41df35050788f96c488d045937904aa59627321
myback <54638513+myback@users.noreply.github.com>
2020-03-01 01:45:20 +0700

    new logging

---
+++ formatters.go
@@ -0,0 +1,44 @@
+package logging
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type Formatter interface {
+	Format(msg LogMessage) (string, error)
+}
+
+type JSONFormatter LogMessage
+type TextFormatter LogMessage
+
+func (j JSONFormatter) Format(msg LogMessage) (string, error) {
+	b, err := json.Marshal(msg)
+	return string(b), err
+}
+
+func (t TextFormatter) Format(msg LogMessage) (string, error) {
+	var buf []string
+
+	fixedKey := []string{
+		"time",
+		"level",
+		"message",
+	}
+
+	tmpMsg := msg
+
+	for _, k := range fixedKey {
+		if v, ok := msg[k]; ok {
+			buf = append(buf, fmt.Sprintf("%s=\"%s\"", k, v))
+			delete(tmpMsg, k)
+		}
+	}
+
+	for k, v := range tmpMsg {
+		buf = append(buf, fmt.Sprintf("%s=\"%s\"", k, v))
+	}
+
+	return strings.Join(buf, " "), nil
+}
--- logging.go
+++ logging.go
@@ -3,171 +3,160 @@ package logging
 import (
 	"fmt"
 	"io"
-	"math/rand"
-	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
-type Logger struct {
-	DebugSet       bool
-	Stdout, Stderr io.Writer
-}
-
-func New(out, e io.Writer, debug bool) *Logger {
+func New(stdout, stderr io.Writer, lvl Level, formatter Formatter) *Logger {
 	return &Logger{
-		DebugSet: debug,
-		Stdout:   out,
-		Stderr:   e,
+		level:  lvl,
+		stdout: stdout,
+		stderr: stderr,
+		fmt:    formatter,
 	}
 }
 
-func (l *Logger) Errorf(format string, msg ...interface{}) {
-	switch len(msg) {
-	case 0:
-		l.Error(format)
-		return
-	case 1:
-		l.Error(fmt.Sprintf(format, msg))
-		return
-	}
+type Level int
 
-	l.stderrWrite("error", fmt.Sprintf(format, msg...))
+const (
+	Fatal Level = iota
+	Error
+	Warning
+	Info
+	Debug
+)
+
+const Message = "message"
+
+type Logger struct {
+	level          Level
+	stdout, stderr io.Writer
+	fmt            Formatter
 }
 
-func (l *Logger) Error(msg interface{}) {
-	l.stderrWrite("error", msgWrap(msg))
-}
+type LogMessage map[string]interface{}
 
-func (l *Logger) Infof(format string, msg ...interface{}) {
-	switch len(msg) {
-	case 0:
-		l.Info(format)
-		return
-	case 1:
-		l.Info(fmt.Sprintf(format, msg))
-		return
+func (l Level) ToString() string {
+	switch l {
+	case Fatal:
+		return "Fatal"
+	case Error:
+		return "Error"
+	case Warning:
+		return "Warning"
+	case Info:
+		return "Info"
+	case Debug:
+		return "Debug"
 	}
 
-	l.stdoutWrite("info", fmt.Sprintf(format, msg...))
+	return ""
+}
+
+func (l *Level) UnmarshalText(lvl []byte) error {
+	switch strings.ToLower(string(lvl)) {
+	case "f", "fatal":
+		*l = Fatal
+	case "e", "err", "error":
+		*l = Error
+	case "w", "warn", "warning":
+		*l = Warning
+	case "i", "info":
+		*l = Info
+	case "d", "dbg", "debug":
+		*l = Debug
+	default:
+		return fmt.Errorf("unknown log level %s", lvl)
+	}
+
+	return nil
+}
+
+func (l *Logger) GetStdout() io.Writer {
+	return l.stdout
+}
+
+func (l *Logger) GetStderr() io.Writer {
+	return l.stderr
+}
+
+func (l *Logger) GetLevel() Level {
+	return l.level
+}
+
+func (l *Logger) IsDebug() bool {
+	return l.level == Debug
+}
+
+func (l *Logger) checkLevel(level Level) bool {
+	return l.level >= level
+}
+
+func (l *Logger) Errorf(msg LogMessage) {
+	l.Log(Error, msg)
+}
+
+func (l *Logger) Error(msg string) {
+	l.Log(Error, LogMessage{Message: msg})
+}
+
+func (l *Logger) Infof(msg LogMessage) {
+	l.Log(Info, msg)
 }
 
 func (l *Logger) Info(msg string) {
-	l.stdoutWrite("info", msgWrap(msg))
+	l.Log(Info, LogMessage{Message: msg})
 }
 
-func (l *Logger) Debugf(format string, msg ...interface{}) {
-	switch len(msg) {
-	case 0:
-		l.Debug(format)
-		return
-	case 1:
-		l.Debug(fmt.Sprintf(format, msg))
-		return
-	}
-
-	if l.DebugSet {
-		l.stdoutWrite("debug", fmt.Sprintf(format, msg...))
-	}
+func (l *Logger) Debugf(msg LogMessage) {
+	l.Log(Debug, msg)
 }
 
 func (l *Logger) Debug(msg string) {
-	if l.DebugSet {
-		l.stdoutWrite("debug", msgWrap(msg))
-	}
+	l.Log(Debug, LogMessage{Message: msg})
 }
 
-func (l *Logger) Fatalf(format string, msg ...interface{}) {
-	switch len(msg) {
-	case 0:
-		l.Fatal(format)
-		return
-	case 1:
-		l.Fatal(fmt.Sprintf(format, msg))
-		return
-	}
-
-	l.stderrWrite("fatal", fmt.Sprintf(format, msg...))
+func (l *Logger) Fatalf(msg LogMessage) {
+	l.Log(Fatal, msg)
 	os.Exit(127)
 }
 
 func (l *Logger) Fatal(msg string) {
-	l.stderrWrite("fatal", msgWrap(msg))
+	l.Log(Fatal, LogMessage{Message: msg})
 	os.Exit(127)
 }
 
-func (l *Logger) Access(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var u string
-		login, _, ok := r.BasicAuth()
-		if ok {
-			u = login
-		} else {
-			u = "-"
-		}
-
-		accessFmt := `user="%s" session_id="%s" address="%s" uri="%s" method="%s" proto="%s" referer="%s" `
-		if l.DebugSet {
-			var hl []string
-			for k, v := range r.Header {
-				hl = append(hl, fmt.Sprintf(`http_%s="%s"`, strings.ToLower(strings.Replace(k, "-", "_", -1)), v[0]))
-			}
-
-			accessFmt = accessFmt + strings.Join(hl, " ")
-		} else {
-			accessFmt = accessFmt + fmt.Sprintf(`useragent="%s"`, r.UserAgent())
-		}
-
-		xSess := GenerateRandString(16)
-		l.stdoutWrite("access", fmt.Sprintf(accessFmt, u, xSess, r.RemoteAddr, r.RequestURI, r.Method, r.Proto, r.Referer()))
-
-		r.Header.Set("X-Session-ID", xSess)
-		next.ServeHTTP(w, r)
-	})
+func (l *Logger) Log(level Level, msg LogMessage) {
+	if l.checkLevel(level) {
+		l.CustomLevelLog(level.ToString(), msg)
+	}
 }
 
-func (l *Logger) stdoutWrite(level, msg string) {
-	_, err := fmt.Fprintf(l.Stdout, `time="%s" level="%s" %s`+"\n", time.Now().UTC().Format("2006-01-02T15:04:05.000Z"), level, msg)
+func (l *Logger) CustomLevelLog(level string, msg LogMessage) {
+	msg["time"] = time.Now().UTC().Format("2006-01-02T15:04:05.000Z")
+	msg["level"] = level
+
+	b, err := l.fmt.Format(msg)
 	if err != nil {
-		fmt.Printf("Log write failed: %s", err)
+		fmt.Fprintf(os.Stderr, "%s ERROR: Log formatter failed: %s", msg["time"], err)
+		return
 	}
 
-}
+	var out io.Writer
+	var lvl Level
 
-func (l *Logger) stderrWrite(level, msg string) {
-	_, err := fmt.Fprintf(l.Stderr, `time="%s" level="%s" %s`+"\n", time.Now().UTC().Format("2006-01-02T15:04:05.000Z"), level, msg)
-	if err != nil {
-		fmt.Printf("Log write failed: %s", err)
-	}
-
-}
-
-func msgWrap(msg interface{}) string {
-	return fmt.Sprintf(`message="%s"`, msg)
-}
-
-func GenerateRandString(n int) string {
-	const (
-		letters       = `1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_`
-		letterIdxBits = 6                    // 6 bits to represent a letter index
-		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-	)
-
-	rand.Seed(time.Now().UnixNano())
-	b := make([]byte, n)
-	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = rand.Int63(), letterIdxMax
+	if err := lvl.UnmarshalText([]byte(level)); err != nil {
+		out = l.stdout
+	} else {
+		if lvl > Error {
+			out = l.stdout
+		} else {
+			out = l.stderr
 		}
-		if idx := int(cache & letterIdxMask); idx < len(letters) {
-			b[i] = letters[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
 	}
 
-	return string(b)
+	if _, err := fmt.Fprintln(out, b); err != nil {
+		fmt.Fprintf(os.Stderr, "%s ERROR: Write log failed: %s", msg["time"], err)
+	}
 }
